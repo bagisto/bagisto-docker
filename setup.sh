f1862719bafb418cf2d684c67f18043ec897acac
Devansh Bawari <devansh.bawari419@webkul.in>
2023-04-25 22:32:16 +0530

    elasticsearch, kibana, redis, mailhog and latest bagisto support added

--- setup.sh
+++ setup.sh
@@ -14,9 +14,15 @@ while ! docker exec ${db_container_id} mysql --user=root --password=root -e "SEL
     sleep 1
 done
 
-# creating empty database
-echo "Creating empty database..."
-while ! docker exec ${db_container_id} mysql --user=root --password=root -e "CREATE DATABASE IF NOT EXISTS bagisto CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci;" >/dev/null 2>&1; do
+# creating empty database for bagisto
+echo "Creating empty database for bagisto..."
+while ! docker exec ${db_container_id} mysql --user=root --password=root -e "CREATE DATABASE bagisto CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci;" >/dev/null 2>&1; do
+    sleep 1
+done
+
+# creating empty database for bagisto testing
+echo "Creating empty database for bagisto testing..."
+while ! docker exec ${db_container_id} mysql --user=root --password=root -e "CREATE DATABASE bagisto_testing CHARACTER SET utf8mb3 COLLATE utf8_unicode_ci;" >/dev/null 2>&1; do
     sleep 1
 done
 
@@ -33,6 +39,7 @@ docker exec -i ${apache_container_id} bash -c "cd bagisto && composer install"
 
 # moving `.env` file
 docker cp .configs/.env ${apache_container_id}:/var/www/html/bagisto/.env
+docker cp .configs/.env.testing ${apache_container_id}:/var/www/html/bagisto/.env.testing
 
 # executing final commands
 docker exec -i ${apache_container_id} bash -c "cd bagisto && php artisan optimize:clear && php artisan migrate:fresh --seed && php artisan storage:link && php artisan bagisto:publish --force && php artisan optimize:clear"
